c62375f7b5d1040561a3ae8c5e822f0efde39b1d
Andreas Axelsson <axl@directivegames.com>
2017-03-31 14:40:51 +0800

    Actually fix iOS build error

--- Plugins/Drift/Source/Drift/Private/Details/PlatformName.cpp
+++ Plugins/Drift/Source/Drift/Private/Details/PlatformName.cpp
@@ -10,31 +10,29 @@ namespace details
 
 const TCHAR* GetPlatformName()
 {
-    static const TCHAR platform_name[] =
 #if PLATFORM_WINDOWS
-    L"Windows";
+    return L"Windows";
 #elif PLATFORM_PS4
-    L"PS4";
+    return L"PS4";
 #elif PLATFORM_XBOXONE
-    L"XBOX1";
+    return L"XBOX1";
 #elif PLATFORM_MAC
-    L"MAC";
+    return L"MAC";
 #elif PLATFORM_IOS
-    FIOSPlatformMisc::GetDefaultDeviceProfileName();
+    return FIOSPlatformMisc::GetDefaultDeviceProfileName();
 #elif PLATFORM_ANDROID
-    L"Android";
+    return L"Android";
 #elif PLATFORM_WINRT_ARM
-    L"WinRT";
+    return L"WinRT";
 #elif PLATFORM_WINRT
-    L"WinRT";
+    return L"WinRT";
 #elif PLATFORM_HTML5
-    L"HTML5";
+    return L"HTML5";
 #elif PLATFORM_LINUX
-    L"Linux";
+    return L"Linux";
 #else
 #error Unknown Platform
 #endif
-    return platform_name;
 }
 
 } // namespace details
