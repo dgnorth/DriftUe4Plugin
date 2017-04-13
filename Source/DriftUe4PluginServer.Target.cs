15e67e171ae5b33f6406a285225b0498328b2e99
Andreas Axelsson <axl@directivegames.com>
2017-04-13 11:17:39 +0800

    Remove use of obsolete function

--- Source/DriftUe4PluginServer.Target.cs
+++ Source/DriftUe4PluginServer.Target.cs
@@ -22,10 +22,4 @@ public class DriftUe4PluginServerTarget : TargetRules
         base.SetupBinaries(Target, ref OutBuildBinaryConfigurations, ref OutExtraModuleNames);
         OutExtraModuleNames.Add("DriftUe4Plugin");
     }
- 
-    public override bool GetSupportedPlatforms(ref List<UnrealTargetPlatform> OutPlatforms)
-    {
-        // It is valid for only server platforms
-        return UnrealBuildTool.UnrealBuildTool.GetAllServerPlatforms(ref OutPlatforms, false);
-    }
 }
